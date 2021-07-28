32f2d600aba18bbf1ed0499a7d7ee7f7aac65703
masinissa <masinissa.hamid@gmail.com>
2021-07-28 16:43:37 +0200

    creation of the sign-up & sign-in routes + gestion of errors (empty fields + user already exists)

--- routes/users.js
+++ routes/users.js
@@ -1,50 +1,85 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 let userModel = require("../models/users");
-var bcrypt = require('bcrypt');
-const uid2 = require('uid2');
+var bcrypt = require("bcrypt");
+const uid2 = require("uid2");
 
 /* GET users listing. */
-router.post('/sign-up', async function(req, res, next) {
-
+router.post("/sign-up", async function (req, res, next) {
   let error = [];
   let result = false;
   let saveUser = null;
   let token = null;
 
   let data = await userModel.findOne({
-    email: req.body.emailFromFront
-  })
+    email: req.body.emailFromFront,
+  });
 
-  if (data != null){
-    error.push("Vous vous êtes déjà enregistré. Vous pouvez vous connecter.")
+  if (
+    req.body.usernameFromFront == "" ||
+    req.body.emailFromFront == "" ||
+    req.body.passwordFromFront == ""
+  ) {
+    error.push("Veuillez remplir tous les champs.");
   }
 
-  if (req.body.usernameFromFront == "" || req.body.emailFromFront == "" || req.body.passwordFromFront ==""){
-    error.push("Veuillez remplir tous les champs.")
+  if (data != null) {
+    error.push("Vous vous êtes déjà enregistré. Vous pouvez vous connecter.");
   }
+
+
 
   if (error.length == 0) {
     let hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
     let newUser = new userModel({
-      firstName : req.body.firstNameFromFront,
-      lastName : req.body.lastNameFromFront,
+      firstName: req.body.firstNameFromFront,
+      lastName: req.body.lastNameFromFront,
       email: req.body.emailFromFront,
       city: req.body.cityFromFront,
       password: hash,
       token: uid2(32),
-    })
+    });
 
     saveUser = await newUser.save();
 
-    if (saveUser){
+    if (saveUser) {
       result = true;
       token = saveUser.token;
     }
   }
 
-  res.json(error, result, saveUser, token);
+  res.json({ error, result, saveUser, token });
+});
+
+router.post("/sign-in", async function (req, res, next) {
+  let error = [];
+  let result = false;
+  let user = null;
+  let token = null;
+
+  if (req.boody.emailFromFront == "" || req.body.passwordFromFront == "") {
+    error.push("Veuillez remplir les deux champs");
+  }
+
+  if (error.length) {
+    let user = await userModel.findOne({
+      email: req.body.emailFromFront,
+    });
+  }
+
+  let password = req.body.passwordFromFront;
+
+  if (user) {
+    if (bcrypt.compareSync(password, user.password)){ 
+      result = true
+        token = user.token
+      } else {
+      error.push("Mot de passe incorrect")
+    }
+  }
+
+  res.json({result});
 });
 
 module.exports = router;
