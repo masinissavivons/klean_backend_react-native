a70499bd254276fdd89ff849b1314b0a52a713c0
Hugo Castarede <hugocastarede@hotmail.fr>
2021-07-28 16:59:22 +0200

    autocomplete done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var request = require('sync-request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/*Autocomplete-search*/
+router.post('/autocomplete-search', function(req, res, next) {
+  
+  console.log(req.body.adress);
+
+  let requete = request("GET", `https://api-adresse.data.gouv.fr/search/?q=${req.body.adress}`);
+  let response = JSON.parse(requete.body);
+
+  res.json({result: true, response: response.features});
 });
 
 module.exports = router;
