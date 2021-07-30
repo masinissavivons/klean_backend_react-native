e0dc704b3e07d734cb8286560876f8d91a136b32
masinissa <masinissa.hamid@gmail.com>
2021-07-30 13:03:32 +0200

    adding city foreign key to user

--- routes/index.js
+++ routes/index.js
@@ -1,74 +1,110 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var request = require('sync-request');
+var request = require("sync-request");
 
-var cleanwalkModel = require('../models/cleanwalks');
+var cleanwalkModel = require("../models/cleanwalks");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 /*Autocomplete-search*/
-router.post('/autocomplete-search', function(req, res, next) {
-  
-  let requete = request("GET", `https://api-adresse.data.gouv.fr/search/?q=${req.body.adress}`);
+router.post("/autocomplete-search", function (req, res, next) {
+  let requete = request(
+    "GET",
+    `https://api-adresse.data.gouv.fr/search/?q=${req.body.adress}`
+  );
   let response = JSON.parse(requete.body);
 
-  res.json({result: true, response: response.features});
+  res.json({ result: true, response: response.features });
 });
 
 /*Autocomplete-search-city-only*/
-router.post('/autocomplete-search-city-only', function(req, res, next) {
-  
-  let cityRegex = /arrondissement/i
+router.post("/autocomplete-search-city-only", function (req, res, next) {
+  let cityRegex = /arrondissement/i;
 
-  let requete = request("GET", `https://api-adresse.data.gouv.fr/search/?q=${req.body.city}&type=municipality`);
+  let requete = request(
+    "GET",
+    `https://api-adresse.data.gouv.fr/search/?q=${req.body.city}&type=municipality`
+  );
   let response = JSON.parse(requete.body);
-  let newResponse = response.features.filter(obj => !cityRegex.test(obj.properties.label) )
-console.log("newResponse", newResponse);
-  newResponse = newResponse.map(obj => {
-    let copy = {...obj}
-    copy.properties.label = copy.properties.city
-    return copy});
+  let newResponse = response.features.filter(
+    (obj) => !cityRegex.test(obj.properties.label)
+  );
+  // console.log("newResponse", newResponse);
+  newResponse = newResponse.map((obj) => {
+    let copy = { ...obj };
+    copy.properties.label = copy.properties.city;
+    return copy;
+  });
 
-  res.json({result: true, newResponse});
+  res.json({ result: true, newResponse });
 });
 
 // load-cleanwalk
-router.get('/load-cleanwalk/:idCW', async function(req, res, next) {
+router.get("/load-cleanwalk/:idCW", async function (req, res, next) {
+  var cleanwalk = await cleanwalkModel
+    .findById(req.params.idCW)
+    .populate("cleanwalkCity")
+    .populate("participantsList")
+    .populate("admin")
+    .exec();
 
-  var cleanwalk = await cleanwalkModel.findById(req.params.idCW).populate('cleanwalkCity').populate('participantsList').populate('admin').exec();
-
-  res.json({result: true, cleanwalk});
-}); 
+  res.json({ result: true, cleanwalk });
+});
 
 /*load-pin-on-change-region*/
-router.post('/load-pin-on-change-region', async function(req, res, next) {
-
+router.post("/load-pin-on-change-region", async function (req, res, next) {
   const coordinateJsonParse = JSON.parse(req.body.coordinate);
   const dateSearch = req.body.date;
 
   //on définit la fonction pour calculer les intervals nécessaires à la requête
   const definePerimeter = (regionLat, regionLong, latD, longD) => {
     let interval = {
-      lat: {min: regionLat - (0.5*latD), max: regionLat + (0.5*latD)},
-      long: {min: regionLong - (0.5*longD), max: regionLong + (0.5*longD)}
+      lat: { min: regionLat - 0.5 * latD, max: regionLat + 0.5 * latD },
+      long: { min: regionLong - 0.5 * longD, max: regionLong + 0.5 * longD },
     };
     return interval;
   };
 
   //on reçoit via le body les éléments de la région qu'on place en arguments de la fonction
-  let customInterval = definePerimeter(coordinateJsonParse.latitude, coordinateJsonParse.longitude, coordinateJsonParse.latitudeDelta, coordinateJsonParse.longitudeDelta);
+  let customInterval = definePerimeter(
+    coordinateJsonParse.latitude,
+    coordinateJsonParse.longitude,
+    coordinateJsonParse.latitudeDelta,
+    coordinateJsonParse.longitudeDelta
+  );
 
   //on fait la requete dans MongoDB
-  let cleanWalkRequest = await cleanwalkModel.find()
-  .where('cleanwalkCoordinates.latitude').gte(customInterval.lat.min).lte(customInterval.lat.max)
-  .where('cleanwalkCoordinates.longitude').gte(customInterval.long.min).lte(customInterval.long.max)
-  .where('startingDate').gte(dateSearch)
-  .populate('admin').exec();
+  let cleanWalkRequest = await cleanwalkModel
+    .find()
+    .where("cleanwalkCoordinates.latitude")
+    .gte(customInterval.lat.min)
+    .lte(customInterval.lat.max)
+    .where("cleanwalkCoordinates.longitude")
+    .gte(customInterval.long.min)
+    .lte(customInterval.long.max)
+    .where("startingDate")
+    .gte(dateSearch)
+    .populate("admin")
+    .exec();
 
-  res.json({result: true, cleanWalkArray: cleanWalkRequest});
+  res.json({ result: true, cleanWalkArray: cleanWalkRequest });
 });
+
+// subscribe to cleanwalk
+// router.post("/subscribe-cw", async function (req, res, next) {
+//   let cleanwalk = await cleanwalkModel.findOne({ _id: idCleanwalk });
+
+//   let idCleanwalk = req.body.cleanwalkIdFromFront;
+
+//   newParticipant = await cleanwalkModel.updateOne(
+//     { _id: idCleanwalk },
+//     { $push: { participantsList: saveUser._id } }
+//   );
+
+//   res.json({ result: true, cleanwalk });
+// });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ let userModel = require("../models/users");
 var bcrypt = require("bcrypt");
 const uid2 = require("uid2");
 const cleanwalkModel = require("../models/cleanwalks");
+const cityModel = require("../models/cities");
 
 function validateEmail(email) {
   const re =
@@ -43,72 +44,185 @@ router.post("/sign-up", async function (req, res, next) {
     error.push("Format d'email incorrect");
   }
 
+  let cityInfo = JSON.parse(req.body.cityInfo);
+  console.log("cityInfo: ", cityInfo);
+  let code = cityInfo.properties.citycode;
+  let coordinates = cityInfo.geometry.coordinates;
+  let population = cityInfo.properties.population;
+
   // register
   if (error.length == 0 && idCleanwalk === undefined) {
-    let hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
-    let newUser = new userModel({
-      firstName: req.body.firstNameFromFront,
-      lastName: req.body.lastNameFromFront,
-      email: req.body.emailFromFront,
-      city: req.body.cityFromFront,
-      password: hash,
-      token: uid2(32),
-    });
+    let found = await cityModel.findOne({ cityCode: code });
 
-    saveUser = await newUser.save();
+    if (found) {
+      let hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
+      let newUser = new userModel({
+        firstName: req.body.firstNameFromFront,
+        lastName: req.body.lastNameFromFront,
+        email: req.body.emailFromFront,
+        city: found._id,
+        password: hash,
+        token: uid2(32),
+      });
 
-    if (saveUser) {
-      result = true;
-      token = saveUser.token;
-    }
+      saveUser = await newUser.save();
 
-    res.json({
-      error,
-      result,
-      saveUser,
-      token,
-    });
-  }
-
-  // register & participate
-  if (error.length == 0 && idCleanwalk !== undefined) {
-    let hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
-    let newUser = new userModel({
-      firstName: req.body.firstNameFromFront,
-      lastName: req.body.lastNameFromFront,
-      email: req.body.emailFromFront,
-      city: req.body.cityFromFront,
-      password: hash,
-      token: uid2(32),
-    });
-
-    let saveUser = await newUser.save();
-
-    if (saveUser) {
-      let cleanwalk = await cleanwalkModel.findOne({ _id: idCleanwalk });
-
-      newParticipant = await cleanwalkModel.updateOne(
-        { _id: idCleanwalk },
-        { $push: { participantsList: saveUser._id } }
-      );
-
-      result = true;
-      token = saveUser.token;
+      if (saveUser) {
+        result = true;
+        token = saveUser.token;
+      }
 
       res.json({
         error,
         result,
         saveUser,
         token,
-        newParticipant,
-      });
+      })
+      return;
     }
+
+    if (found == null) {
+      let newCity = new cityModel({
+        cityName: req.body.cityFromFront,
+        cityCoordinates: {
+          longitude: coordinates[0],
+          latitude: coordinates[1],
+        },
+        population: population,
+        cityCode: code,
+      });
+      let citySaved = await newCity.save();
+
+      let hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
+      let newUser = new userModel({
+        firstName: req.body.firstNameFromFront,
+        lastName: req.body.lastNameFromFront,
+        email: req.body.emailFromFront,
+        city: citySaved._id,
+        password: hash,
+        token: uid2(32),
+      });
+
+      saveUser = await newUser.save();
+
+      if (saveUser) {
+        result = true;
+        token = saveUser.token;
+      }
+
+      res.json({
+        error,
+        result,
+        saveUser,
+        token,
+      })
+      return;
+    }
+
+    res.json({
+      error,
+      result,
+    })
+    return;
   }
 
-  res.json({
-    error,
-    result,
-  });
+  // register & participate
+  if (error.length == 0 && idCleanwalk !== undefined) {
+    let found = await cityModel.findOne({ cityCode: code });
+
+    if (found) {
+      let hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
+      let newUser = new userModel({
+        firstName: req.body.firstNameFromFront,
+        lastName: req.body.lastNameFromFront,
+        email: req.body.emailFromFront,
+        city: found._id,
+        password: hash,
+        token: uid2(32),
+      });
+
+      let saveUser = await newUser.save();
+
+      if (saveUser) {
+        let cleanwalk = await cleanwalkModel.findOne({ _id: idCleanwalk });
+
+        newParticipant = await cleanwalkModel.updateOne(
+          { _id: idCleanwalk },
+          { $push: { participantsList: saveUser._id } }
+        );
+
+        result = true;
+        token = saveUser.token;
+
+        res.json({
+          error,
+          result,
+          saveUser,
+          token,
+          newParticipant,
+        })
+        return;
+      } else {
+        res.json({
+          error,
+          result,
+        })
+        return;
+      }
+    }
+
+    if (found == null) {
+      let newCity = new cityModel({
+        cityName: req.body.cityFromFront,
+        cityCoordinates: {
+          longitude: coordinates[0],
+          latitude: coordinates[1],
+        },
+        population: population,
+        cityCode: code,
+      });
+      let citySaved = await newCity.save();
+
+      let hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
+      let newUser = new userModel({
+        firstName: req.body.firstNameFromFront,
+        lastName: req.body.lastNameFromFront,
+        email: req.body.emailFromFront,
+        city: found._id,
+        password: hash,
+        token: uid2(32),
+      });
+
+      let saveUser = await newUser.save();
+
+      if (saveUser) {
+        let cleanwalk = await cleanwalkModel.findOne({ _id: idCleanwalk });
+
+        newParticipant = await cleanwalkModel.updateOne(
+          { _id: idCleanwalk },
+          { $push: { participantsList: saveUser._id } }
+        );
+
+        result = true;
+        token = saveUser.token;
+
+        res.json({
+          error,
+          result,
+          saveUser,
+          token,
+          newParticipant,
+        });
+        return;
+      } else {
+        res.json({
+          error,
+          result,
+        });
+        return;
+      }
+    }
+  }
 });
 
 //    POST Sign-in    //
@@ -144,6 +258,7 @@ router.post("/sign-in", async function (req, res, next) {
     }
 
     res.json({ error, result, user, token });
+    return;
   }
 
   // sign-in & participate
@@ -152,7 +267,6 @@ router.post("/sign-in", async function (req, res, next) {
       email: req.body.emailFromFront,
     });
 
-    console.log("user: ", user)
     if (user) {
       if (bcrypt.compareSync(req.body.passwordFromFront, user.password)) {
         result = true;
@@ -167,7 +281,7 @@ router.post("/sign-in", async function (req, res, next) {
       } else {
         result = false;
         error.push("Mot de passe incorrect.");
-      } 
+      }
     } else {
       error.push("Email incorrect.");
     }
